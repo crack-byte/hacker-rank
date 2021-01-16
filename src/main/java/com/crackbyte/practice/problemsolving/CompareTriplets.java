d897457f457beb4125e4083c76db22e833aeddb0
abhishek <abhishek>
2021-01-16 17:20:22 +0530

    updated a question

---
+++ src/main/java/com/crackbyte/practice/problemsolving/CompareTriplets.java
@@ -0,0 +1,54 @@
+package com.crackbyte.practice.problemsolving;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CompareTriplets {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int tempA = 0;
+        int tempB = 0;
+        for (int i = a.size() - 1; i >= 0; i--) {
+            if (a.get(i) > b.get(i)) {
+                tempA++;
+            } else if (a.get(i) < b.get(i)) {
+                tempB++;
+            }
+        }
+        List<Integer> result = new ArrayList<>(2);
+        result.add(tempA);
+        result.add(tempB);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
